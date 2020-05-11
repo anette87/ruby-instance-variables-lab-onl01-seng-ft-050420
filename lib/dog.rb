503cea96394d349e20fc5a611f204f4769588833
Anette Rivera <anettem.rivera@gmail.com>
2020-05-11 20:07:10 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+  def name= (dog_name)  #writing, setter. Ex: name=, person=
+    this_dogs_name = dog_name 
+  end
+  
+  def name  # reading, setting
+    this_dogs_name
+  end 
+end 
+
+lassie = Dog.new 
+lassie.name = ("Lassie")
+return lassie.name 
